13e43b4dc993957cf214e6bfb31ea94cc8aa2661
Matt Readout <matt.readout@gmail.com>
2019-02-04 12:23:17 -0500

    update artist serializer

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,5 +1,5 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :media, :bio, :img_url
+  attributes :id, :name, :location, :media, :bio, :img_url, :twitter, :instagram
 
 	has_many :works
 end
